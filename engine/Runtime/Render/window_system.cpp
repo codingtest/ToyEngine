0fb104e39ca364660650e02db40a6b38e75ea664
rongyang <rongyang@tencent.com>
2022-07-05 14:21:38 +0800

    去掉glfw依赖

--- engine/Runtime/Render/window_system.cpp
+++ engine/Runtime/Render/window_system.cpp
@@ -6,51 +6,51 @@ namespace ToyEngine
 {
     WindowSystem::~WindowSystem()
     {
-        glfwDestroyWindow(m_window);
-        glfwTerminate();
+        //glfwDestroyWindow(m_window);
+        //glfwTerminate();
     }
 
     void WindowSystem::initialize(WindowCreateInfo create_info)
     {
-        if (!glfwInit())
-        {
-           // LOG_FATAL(__FUNCTION__, "failed to initialize GLFW");
-            return;
-        }
+        //if (!glfwInit())
+        //{
+        //   // LOG_FATAL(__FUNCTION__, "failed to initialize GLFW");
+        //    return;
+        //}
 
         m_width  = create_info.width;
         m_height = create_info.height;
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        m_window = glfwCreateWindow(create_info.width, create_info.height, create_info.title, nullptr, nullptr);
-        if (!m_window)
-        {
-           // LOG_FATAL(__FUNCTION__, "failed to create window");
-            glfwTerminate();
-            return;
-        }
+        //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        //m_window = glfwCreateWindow(create_info.width, create_info.height, create_info.title, nullptr, nullptr);
+        //if (!m_window)
+        //{
+        //   // LOG_FATAL(__FUNCTION__, "failed to create window");
+        //    glfwTerminate();
+        //    return;
+        //}
 
-        // Setup input callbacks
-        glfwSetWindowUserPointer(m_window, this);
-        glfwSetKeyCallback(m_window, keyCallback);
-        glfwSetCharCallback(m_window, charCallback);
-        glfwSetCharModsCallback(m_window, charModsCallback);
-        glfwSetMouseButtonCallback(m_window, mouseButtonCallback);
-        glfwSetCursorPosCallback(m_window, cursorPosCallback);
-        glfwSetCursorEnterCallback(m_window, cursorEnterCallback);
-        glfwSetScrollCallback(m_window, scrollCallback);
-        glfwSetDropCallback(m_window, dropCallback);
-        glfwSetWindowSizeCallback(m_window, windowSizeCallback);
-        glfwSetWindowCloseCallback(m_window, windowCloseCallback);
+        //// Setup input callbacks
+        //glfwSetWindowUserPointer(m_window, this);
+        //glfwSetKeyCallback(m_window, keyCallback);
+        //glfwSetCharCallback(m_window, charCallback);
+        //glfwSetCharModsCallback(m_window, charModsCallback);
+        //glfwSetMouseButtonCallback(m_window, mouseButtonCallback);
+        //glfwSetCursorPosCallback(m_window, cursorPosCallback);
+        //glfwSetCursorEnterCallback(m_window, cursorEnterCallback);
+        //glfwSetScrollCallback(m_window, scrollCallback);
+        //glfwSetDropCallback(m_window, dropCallback);
+        //glfwSetWindowSizeCallback(m_window, windowSizeCallback);
+        //glfwSetWindowCloseCallback(m_window, windowCloseCallback);
 
-        glfwSetInputMode(m_window, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+        //glfwSetInputMode(m_window, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
     }
 
-    void WindowSystem::pollEvents() const { glfwPollEvents(); }
+    void WindowSystem::pollEvents() const {/* glfwPollEvents();*/ }
 
-    bool WindowSystem::shouldClose() const { return glfwWindowShouldClose(m_window); }
+    bool WindowSystem::shouldClose() const { return false;/*return glfwWindowShouldClose(m_window);*/ }
 
-    void WindowSystem::setTile(const char* title) { glfwSetWindowTitle(m_window, title); }
+    void WindowSystem::setTile(const char* title) {/* glfwSetWindowTitle(m_window, title);*/ }
 
     GLFWwindow* WindowSystem::getWindow() const { return m_window; }
 
@@ -59,6 +59,6 @@ namespace ToyEngine
     void WindowSystem::setFocusMode(bool mode)
     {
         m_is_focus_mode = mode;
-        glfwSetInputMode(m_window, GLFW_CURSOR, m_is_focus_mode ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
+       // glfwSetInputMode(m_window, GLFW_CURSOR, m_is_focus_mode ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL);
     }
 } // namespace ToyEngine
